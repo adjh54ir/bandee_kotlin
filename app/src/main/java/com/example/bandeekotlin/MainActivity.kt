0ebb1b30f68a0a2e727845f9401b94ebd8c9b21d
Lee <adjh54ir@gmail.com>
2022-08-29 13:44:38 +0900

    camera commit

--- app/src/main/java/com/example/bandeekotlin/MainActivity.kt
+++ app/src/main/java/com/example/bandeekotlin/MainActivity.kt
@@ -1,41 +1,130 @@
 package com.example.bandeekotlin
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
-import android.os.Debug
 import android.util.Base64
 import android.util.Base64.*
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.bandeekotlin.*
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.bandeekotlin.`interface`.ImageApi
 import com.example.bandeekotlin.model.PostImage
 import com.example.bandeekotlin.model.ResponseCode
 import okhttp3.OkHttpClient
-import org.json.JSONObject
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.*
+import java.nio.ByteBuffer
 import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
 
 
 class MainActivity : AppCompatActivity() {
-
     // 시스템 환경설정 - 네트워크에 나와있는 IP주소에 따라서 지정을 한다.
-//    private val API_BASE_URL = "http://10.2.2.0:5000"       // 시뮬레이터
     private val API_BASE_URL = "http://192.168.0.4:5000"  // 로컬 디바이스
+
+    companion object {
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExcutor: ExecutorService
 
     // 메인 페이지
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        createRetrofitApi(); // Retrofit2
+//        createRetrofitApi(); // Retrofit2
+        connectionCamera();
     }
+
+    /**
+     * 카메라 연결
+     */
+    private fun connectionCamera() {
+
+        // [STEP1] 카메라 권한 체크
+        if (allPermissionGranted()) {
+            startCamera()
+        } else {
+            // 권한 요청
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSION, REQUEST_CODE_PERMISSIONS)
+        }
+//        // 캡처 버튼
+//        camera_capture_button.setOnClickListener { takePhoto() }
+
+        // 저장할 디렉토리
+        outputDirectory = getOutputDirectory()
+
+        // single thread
+        cameraExcutor = Executors.newSingleThreadExecutor()
+    }
+
+    /**
+     * 권한 승인 결과
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "권한 승인을 하지 않았습니다.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
+    /**
+     * 카메라 시작
+     */
+    private fun startCamera() {
+    }
+
+    /**
+     * 권한 체크
+     */
+    private fun allPermissionGranted() = REQUIRED_PERMISSION.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply {
+                mkdir()
+            }
+        }
+        return if (mediaDir != null && mediaDir.exists()) {
+            mediaDir
+        } else {
+            filesDir
+        }
+    }
+
+
+    // ============================================================================================
 
     /**
      * Retrofit을 이용한 API 통신 테스트
@@ -48,16 +137,15 @@ class MainActivity : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val service = imageService.create(ImageApi::class.java)
-
+        // API 로 반복 통신 수행
         val mt = measureTimedValue {
             // 1초에 2번씩 수행
-            for (i in 1..120) {
+            for (i in 1..10) {
                 Log.d("시간 시간 ::", "${System.currentTimeMillis()}");
-                postBase64(service);
+                postBase64(service);        // API 호출
             }
         }
         Log.d("측정 시간!!!!", "${mt}");
-
     }
 
 
@@ -96,10 +184,7 @@ class MainActivity : AppCompatActivity() {
      */
     private fun postBase64(service: ImageApi) {
         val base64Str = imageToBase64();
-
         val tempImageName = "image1";
-
-
         // Image To base64
         val formData = PostImage(base64Str, tempImageName)
         service.postBase64(formData)
